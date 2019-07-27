48037eab3b9d79125943efd2d29bf96f7ab6b3de
Yunus Ayar <greybley@yahoo.de>
2019-07-27 16:19:10 +0200

    Update style

--- base64/base64.hpp
+++ base64/base64.hpp
@@ -8,7 +8,7 @@
 class base64_error : public std::runtime_error
 {
 public:
-	std::runtime_error::runtime_error;
+	using std::runtime_error::runtime_error;
 };
 
 class base64
@@ -23,175 +23,179 @@ public:
 		URL_FILENAME_SAFE
 	};
 
-	static void decode_inplace(std::string & _str, ALPHABET _alphabet = ALPHABET::AUTO)
+	static void decode_inplace(std::string& str, ALPHABET alphabet = ALPHABET::AUTO)
 	{
-		_str.resize(decode_inplace(&_str[0], _str.length(), _alphabet));
+		str.resize(decode_inplace(&str[0], str.length(), alphabet));
 	}
-	static size_t decode_inplace(char * _str, size_t _length, ALPHABET _alphapet = ALPHABET::AUTO)
+
+	static size_t decode_inplace(char* str, size_t length, ALPHABET alphapet = ALPHABET::AUTO)
 	{
-		if (!_length) {
+		if (!length) {
 			return 0;
-		} else if (_length % 4) {
+		} else if (length % 4) {
 			throw base64_error("invalid base64 input.");
 		}
 
-		return _length / 4 * 3 - do_decode(_str, _length, _str, _alphapet);
+		return length / 4 * 3 - do_decode(str, length, str, alphapet);
 	}
-	static std::string encode(const std::string & _str, ALPHABET _alphabet = ALPHABET::NORMAL)
+
+	static std::string encode(const std::string& str, ALPHABET alphabet = ALPHABET::NORMAL)
 	{
-		return encode(_str.c_str(), _str.length(), _alphabet);
+		return encode(str.c_str(), str.length(), alphabet);
 	}
-	static std::string encode(const char * _str, size_t _length, ALPHABET _alphabet = ALPHABET::NORMAL)
+
+	static std::string encode(const char* str, size_t length, ALPHABET alphabet = ALPHABET::NORMAL)
 	{
-		if (!_length) {
+		if (!length) {
 			return "";
 		}
 
-		std::string _result;
-		const char * _alpha = nullptr;
-		const auto _end = _str + _length;
+		std::string result;
+		const char* alpha = nullptr;
+		const auto end = str + length;
 
 		// Set alphabet
-		switch (_alphabet) {
+		switch (alphabet) {
 		case ALPHABET::AUTO:
 		case ALPHABET::NORMAL:
-			_alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+			alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
 			break;
 		case ALPHABET::URL_FILENAME_SAFE:
-			_alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
+			alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
 
 			break;
 		}
 
-		_result.resize((_length / 3 + (_length % 3 ? 1 : 0)) * 4);
+		result.resize((length / 3 + (length % 3 ? 1 : 0)) * 4);
 
-		auto _out = &_result[0];
+		auto out = &result[0];
 
 		// Encode 3 bytes
-		while (_str + 3 <= _end) {
-			encode<3>(_alpha, _str, _out);
+		while (str + 3 <= end) {
+			encode<3>(alpha, str, out);
 		}
 
 		// Encode last bit
-		if (_end - _str == 1) {
-			encode<1>(_alpha, _str, _out);
-		} else if (_end - _str == 2) {
-			encode<2>(_alpha, _str, _out);
+		if (end - str == 1) {
+			encode<1>(alpha, str, out);
+		} else if (end - str == 2) {
+			encode<2>(alpha, str, out);
 		}
 
-		return _result;
+		return result;
 	}
-	static std::string decode(const std::string & _str, ALPHABET _alphabet = ALPHABET::AUTO)
+
+	static std::string decode(const std::string& str, ALPHABET alphabet = ALPHABET::AUTO)
 	{
-		return decode(_str.c_str(), _str.length(), _alphabet);
+		return decode(str.c_str(), str.length(), alphabet);
 	}
-	static std::string decode(const char * _str, size_t _length, ALPHABET _alphabet = ALPHABET::AUTO)
+
+	static std::string decode(const char* str, size_t length, ALPHABET alphabet = ALPHABET::AUTO)
 	{
-		if (!_length) {
+		if (!length) {
 			return "";
-		} else if (_length % 4) {
+		} else if (length % 4) {
 			throw base64_error("invalid base64 input.");
 		}
 
-		std::string _result;
-		const auto _end = _str + _length;
+		std::string result;
 
-		_result.resize(_length / 4 * 3);
-		_result.resize(_result.length() - do_decode(_str, _length, &_result[0], _alphabet));
+		result.resize(length / 4 * 3);
+		result.resize(result.length() - do_decode(str, length, &result[0], alphabet));
 
-		return _result;
+		return result;
 	}
 
 private:
 	template<int Count>
-	static void encode(const char * _alphabet, const char *& _input, char *& _output)
+	static void encode(const char* alphabet, const char*& input, char*& output)
 	{
 		if (Count == 3) {
-			_output[0] = _alphabet[_input[0] >> 2 & 0x3f];
-			_output[1] = _alphabet[(_input[0] & 0x03) << 4 | _input[1] >> 4 & 0x0f];
-			_output[2] = _alphabet[(_input[1] & 0x0f) << 2 | _input[2] >> 6 & 0x03];
-			_output[3] = _alphabet[_input[2] & 0x3f];
+			output[0] = alphabet[input[0] >> 2 & 0x3f];
+			output[1] = alphabet[(input[0] & 0x03) << 4 | input[1] >> 4 & 0x0f];
+			output[2] = alphabet[(input[1] & 0x0f) << 2 | input[2] >> 6 & 0x03];
+			output[3] = alphabet[input[2] & 0x3f];
 		} else if (Count == 2) {
-			_output[0] = _alphabet[_input[0] >> 2 & 0x3f];
-			_output[1] = _alphabet[(_input[0] & 0x03) << 4 | _input[1] >> 4 & 0x0f];
-			_output[2] = _alphabet[(_input[1] & 0x0f) << 2];
-			_output[3] = pad;
+			output[0] = alphabet[input[0] >> 2 & 0x3f];
+			output[1] = alphabet[(input[0] & 0x03) << 4 | input[1] >> 4 & 0x0f];
+			output[2] = alphabet[(input[1] & 0x0f) << 2];
+			output[3] = pad;
 		} else if (Count == 1) {
-			_output[0] = _alphabet[_input[0] >> 2 & 0x3f];
-			_output[1] = _alphabet[(_input[0] & 0x03) << 4];
-			_output[2] = pad;
-			_output[3] = pad;
+			output[0] = alphabet[input[0] >> 2 & 0x3f];
+			output[1] = alphabet[(input[0] & 0x03) << 4];
+			output[2] = pad;
+			output[3] = pad;
 		}
 
-		_input += Count;
-		_output += 4;
+		input += Count;
+		output += 4;
 	}
 	template<int Count>
-	static void decode(ALPHABET & _alphabet, const char *& _input, char *& _output)
+	static void decode(ALPHABET& alphabet, const char*& input, char*& output)
 	{
-		char _tmp;
+		char tmp;
 
-		_output[0] = base64_value(_alphabet, _input[0]) << 2;
+		output[0] = base64_value(alphabet, input[0]) << 2;
 
-		_tmp = base64_value(_alphabet, _input[1]);
+		tmp = base64_value(alphabet, input[1]);
 
-		_output[0] |= _tmp >> 4;
+		output[0] |= tmp >> 4;
 
 		if (Count < 2) {
-			_output[1] = (_tmp & 0x0f) << 4;
+			output[1] = (tmp & 0x0f) << 4;
 
-			_tmp = base64_value(_alphabet, _input[2]);
+			tmp = base64_value(alphabet, input[2]);
 
-			_output[1] |= _tmp >> 2;
+			output[1] |= tmp >> 2;
 
 			if (Count < 1) {
-				_output[2] = (_tmp & 0x03) << 6;
-				_output[2] |= base64_value(_alphabet, _input[3]);
+				output[2] = (tmp & 0x03) << 6;
+				output[2] |= base64_value(alphabet, input[3]);
 			}
 		}
 
-		_input += 4;
-		_output += 3 - Count;
+		input += 4;
+		output += 3 - Count;
 	}
-	static char base64_value(ALPHABET & _alphabet, char _char)
+	static char base64_value(ALPHABET& alphabet, char c)
 	{
-		if (_char >= 'A' && _char <= 'Z') {
-			return _char - 'A';
-		} else if (_char >= 'a' && _char <= 'z') {
-			return _char - 'a' + 26;
-		} else if (_char >= '0' && _char <= '9') {
-			return _char - '0' + 52;
+		if (c >= 'A' && c <= 'Z') {
+			return c - 'A';
+		} else if (c >= 'a' && c <= 'z') {
+			return c - 'a' + 26;
+		} else if (c >= '0' && c <= '9') {
+			return c - '0' + 52;
 		}
 
 		// Comes down to alphabet
-		if (_alphabet == ALPHABET::NORMAL) {
-			if (_char == '+') {
+		if (alphabet == ALPHABET::NORMAL) {
+			if (c == '+') {
 				return 62;
-			} else if (_char == '/') {
+			} else if (c == '/') {
 				return 63;
 			}
-		} else if (_alphabet == ALPHABET::URL_FILENAME_SAFE) {
-			if (_char == '-') {
+		} else if (alphabet == ALPHABET::URL_FILENAME_SAFE) {
+			if (c == '-') {
 				return 62;
-			} else if (_char == '_') {
+			} else if (c == '_') {
 				return 63;
 			}
 		} else {
-			if (_char == '+') {
-				_alphabet = ALPHABET::NORMAL;
+			if (c == '+') {
+				alphabet = ALPHABET::NORMAL;
 
 				return 62;
-			} else if (_char == '/') {
-				_alphabet = ALPHABET::NORMAL;
+			} else if (c == '/') {
+				alphabet = ALPHABET::NORMAL;
 
 				return 63;
-			} else if (_char == '-') {
-				_alphabet = ALPHABET::URL_FILENAME_SAFE;
+			} else if (c == '-') {
+				alphabet = ALPHABET::URL_FILENAME_SAFE;
 
 				return 62;
-			} else if (_char == '_') {
-				_alphabet = ALPHABET::URL_FILENAME_SAFE;
+			} else if (c == '_') {
+				alphabet = ALPHABET::URL_FILENAME_SAFE;
 
 				return 63;
 			}
@@ -199,29 +203,30 @@ private:
 
 		throw base64_error("invalid base64 character.");
 	}
-	static size_t do_decode(const char * _input, size_t _length, char * _output, ALPHABET _alphabet)
+
+	static size_t do_decode(const char* input, size_t length, char* output, ALPHABET alphabet)
 	{
-		const auto _end = _input + _length - 4;
+		const auto end = input + length - 4;
 
 		// Decode 4 bytes
-		while (_input + 4 <= _end) {
-			decode<0>(_alphabet, _input, _output);
+		while (input + 4 <= end) {
+			decode<0>(alphabet, input, output);
 		}
 
 		// Decode last 4 bytes
-		if (_input[3] == pad) {
-			if (_input[2] == pad) {
-				decode<2>(_alphabet, _input, _output);
+		if (input[3] == pad) {
+			if (input[2] == pad) {
+				decode<2>(alphabet, input, output);
 
 				return 2;
 			}
 
-			decode<1>(_alphabet, _input, _output);
+			decode<1>(alphabet, input, output);
 
 			return 1;
 		}
 
-		decode<0>(_alphabet, _input, _output);
+		decode<0>(alphabet, input, output);
 
 		return 0;
 	}
